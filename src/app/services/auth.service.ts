706e6e86c3d17a9a62bbed8e9f6b1611485697be
Nacho <ignaciogiralte@gmail.com>
2022-03-07 17:32:47 +0100

    firebase posted

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs/operators'
+import { Usuario } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public auth: AngularFireAuth) { }
+  constructor(
+    public auth: AngularFireAuth,
+    public firestore: AngularFirestore
+    ) { }
 
 
   initAuthListener() {
@@ -18,13 +24,14 @@ export class AuthService {
     })
   }
 
-
-
-
-
   crearUsuario(nombre: string, email: string, password: string) {
     // console.log("Data del servicio Auth:", nombre, email, password );
-    return this.auth.createUserWithEmailAndPassword(email, password);
+    return this.auth.createUserWithEmailAndPassword(email, password)
+    .then (fbUser => {
+      const newUser = new Usuario (fbUser.user!.uid, nombre, email)
+      return this.firestore.doc(`${fbUser.user!.uid}/usuario`).set({...newUser})
+      
+    });
   }
   loginUsuario(email: string, password: string) {
     console.log(email, password);
@@ -33,6 +40,11 @@ export class AuthService {
 
   logOut() {
     return this.auth.signOut();
+  }
+  isAuth(){
+    return this.auth.authState.pipe(
+      map (fbUser => fbUser !=null)
+    );
   }
 
 }
